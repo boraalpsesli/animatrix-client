b30b4bdf1fdaa788803906c8536e48aa2809d953
Bora Alp Sesli <boraalpsesli@hotmail.com>
2019-11-22 18:23:20 +0300

    CARD İMPLEMENTED

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./navbar";
+import Card from "./card";
 
 function App() {
-  return <div>Hello World!!!</div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Card />
+    </div>
+  );
 }
 
 export default App;
